96df211c53f63795eec3838c36a9fee37b6e96d7
maryambou <mbouguer@uci.edu>
2026-08-31 16:53:50 -0700

    Time: 15 ms (9.39%), Space: 13.4 MB (37.71%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,58 +1,16 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char, int> values = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+
         int result = 0;
+
         for(int i = 0; i < s.length(); i++){
-            if(s[i] == 'I'){
-                if(s[i+1] == 'V'){
-                    result += 4;
-                    i++;
-                }
-                else if(s[i+1] == 'X'){
-                    result += 9;
-                    i++;
-                }
-                else{
-                    result += 1;
-                }
+            if((i < s.length()-1) && (values[s[i]] < values[s[i+1]])){
+                result -= values[s[i]];
             }
-            else if(s[i] == 'V'){
-                result += 5;
-            }
-            else if(s[i] == 'X'){
-                if(s[i+1] == 'L'){
-                    result += 40;
-                    i++;
-                }
-                else if(s[i+1] == 'C'){
-                    result += 90;
-                    i++;
-                }
-                else{
-                    result += 10;
-                }
-            }
-            else if(s[i] == 'L'){
-                result += 50;
-            }
-            else if(s[i] == 'C'){
-                if(s[i+1] == 'D'){
-                    result += 400;
-                    i++;
-                }
-                else if(s[i+1] == 'M'){
-                    result += 900;
-                    i++;
-                }
-                else{
-                    result += 100;
-                }
-            }
-            else if(s[i] == 'D'){
-                result += 500;
-            }
-            else if(s[i] == 'M'){
-                result += 1000;
+            else{
+                result += values[s[i]];
             }
         }
         return result;
